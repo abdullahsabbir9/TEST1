94f3cb5763842a176fceb931e82b08fef5b502e1
Abdullah Sabbir <abdullahsabbir9@gmail.com>
2022-02-13 02:49:11 -0500

    First commit

---
+++ eclipse-workspace/triangle/src/triangletest/TriTest.java
@@ -0,0 +1,21 @@
+package triangletest;
+
+public class TriTest {
+	
+	public static void main(String[] args) {
+		System.out.println("     *     ");
+		System.out.println("    * *    ");
+		System.out.println("   *   *   ");
+		System.out.println("  *     *  ");
+		System.out.println(" ********* ");
+		
+		
+		System.out.println(" ********* ");
+		System.out.println(" *       * ");
+		System.out.println(" *       * ");
+		System.out.println(" ********* ");
+		
+		
+	}
+
+}
